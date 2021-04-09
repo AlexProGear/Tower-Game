f6c3680ff8dccdecc560cd8bc4082de0a77dd921
AlexProgrammer <alexprogrammer74@gmail.com>
2021-04-09 14:00:53 +0500

    Camera follow script

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float followSpeed;
+
+    private void Start()
+    {
+        UpdatePositionForNewTarget();
+    }
+
+    private void UpdatePositionForNewTarget()
+    {
+        transform.position = targetTransform.position + offset;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetTransform.position + offset, Time.deltaTime * followSpeed);
+    }
+}
