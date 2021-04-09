e2ae997488aff58b86fc2fbec23e892d249a6469
AlexProgrammer <alexprogrammer74@gmail.com>
2021-04-09 13:52:20 +0500

    Player movement (NavMesh & Animator)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera mainCamera;
+    private NavMeshAgent navAgent;
+    private Animator animator;
+    private static readonly int SpeedPropertyID = Animator.StringToHash("Speed");
+
+    private void Start()
+    {
+        // Caching necessary components
+        mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        // Set new destination on click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray lookDirRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(lookDirRay, out RaycastHit hitInfo))
+            {
+                navAgent.SetDestination(hitInfo.point);
+            }
+        }
+        
+        // Update animation values
+        float movementSpeed = navAgent.desiredVelocity.magnitude;
+        animator.SetFloat(SpeedPropertyID, movementSpeed);
+    }
+}
