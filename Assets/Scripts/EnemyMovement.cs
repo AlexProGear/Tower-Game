a55425b0b5f3c820618e4f8f7ac081ce6a9cd7d8
AlexProgrammer <alexprogrammer74@gmail.com>
2021-04-09 14:21:06 +0500

    Enemy follow logic & animation

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float updateTargetInterval = 0.5f;
+    
+    private Transform playerTransform;
+    private NavMeshAgent navAgent;
+    private Animator animator;
+    private static readonly int MovePropertyID = Animator.StringToHash("Move");
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.FindWithTag("Player").transform;
+        StartCoroutine(UpdateTargetPositionPeriodically());
+    }
+
+    private IEnumerator UpdateTargetPositionPeriodically()
+    {
+        while (gameObject.activeSelf)
+        {
+            navAgent.SetDestination(playerTransform.position);
+            yield return new WaitForSeconds(updateTargetInterval);
+        }
+    }
+
+    private void Update()
+    {
+        // Update animation values
+        float movementSpeed = navAgent.desiredVelocity.magnitude;
+        animator.SetBool(MovePropertyID, movementSpeed > Mathf.Epsilon);
+    }
+}
